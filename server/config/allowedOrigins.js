e6aa4473ea7c6790df51782a2b2b1db263403ce8
HenskiS <59155518+HenskiS@users.noreply.github.com>
2024-05-07 12:34:07 -0700

    Update allowedOrigins.js

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
-    'https://www.yoursite.com',
+    'https://www.cigarlift.work',
+    'https://cigarlift.work',
     'http://127.0.0.1:5500',
     'http://localhost:3500',
     'http://localhost:3000',
